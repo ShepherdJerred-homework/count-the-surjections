1b394dab22a50a569941909cfa44d47136f51552
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-08-22 15:50:35 -0500

    finish impl

--- src/Surjections.java
+++ src/Surjections.java
@@ -1,4 +1,4 @@
-// Surjections
+// surjections
 // Jerred Shepherd
 
 import java.io.File;
@@ -20,7 +20,6 @@ public class Surjections {
             String line = scanner.nextLine();
             String[] splitLine = line.split(" ");
 
-
             m = Integer.valueOf(splitLine[0]);
             n = Integer.valueOf(splitLine[1]);
 
@@ -28,17 +27,7 @@ public class Surjections {
                 break;
             }
 
-            int count = -1;
-
-            if (n == 1) {
-                count = 1;
-            } else if (m < n) {
-                count = 0;
-            } else if (m == n) {
-                // count = m!
-            } else {
-                // lots of math
-            }
+            long count = solve(m, n);
 
             String output = String.format("S(%s,%s) = %s", m, n, count);
             System.out.println(output);
@@ -46,5 +35,40 @@ public class Surjections {
         }
 
         printWriter.close();
+    }
+
+    private static long solve(long m, long n) {
+        long count;
+
+        if (n == 1) {
+            count = 1;
+        } else if (m < n) {
+            count = 0;
+        } else if (m == n) {
+            count = 1;
+            for (int i = 0; i < m -1; i++) {
+                count *= i;
+            }
+        } else {
+            count = sum(m, n);
+        }
+
+        return count;
+    }
+
+    private static long sum(long m, long n) {
+        int sum = 0;
+        for (int i = 1; i <= n - 1; i++) {
+            sum += math(n, i) * solve(m, i);
+        }
+        return sum;
+    }
+
+    private static long math(long n, long i) {
+        if (i == 0 || n == 0 || i == n) {
+            return 1;
+        } else {
+            return math(n - 1, i - 1) + math(n - 1, i);
+        }
     }
 }
